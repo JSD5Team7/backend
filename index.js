4f62a4de629a2556732a0c217fd84c0dcbc8327f
Chakrit <chakritkp@hotmail.com>
2023-10-09 17:10:24 +0700

    delete console.log

--- index.js
+++ index.js
@@ -68,7 +68,6 @@ app.get("/tennisCourt/:court/:day",async (req,res)=>{
 app.get("/coachList",async (req,res)=>{
   try {
     const data = await coachList.find({});
-    console.log(data.toString());
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({message: error.message});
