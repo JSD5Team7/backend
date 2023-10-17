f404404245b66d81ca6555fef95fe9423a23c93a
thanachot30 <su.thanachot@gmail.com>
2023-10-17 10:05:52 +0700

    edit sport

--- routes/User.js
+++ routes/User.js
@@ -8,7 +8,7 @@ Router.get("/history/:user_id",async (req,res)=>{
     try {
       const user_id = req.params.user_id;
       const data = await txdata.find({"information.user_id":user_id});
-      console.log(data);
+    //   console.log(data);
       return res.status(200).json(data);
     } catch (error) {
       return res.status(500).json({message: error.message});
